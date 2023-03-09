e9c6e51c719c4011c6587ed8e39da4ec7817cf8a
Gabriella <gabriellalok3@gmail.com>
2023-03-09 11:52:49 -0300

    conectando ao mongodb e populando com dados de User

--- src/main/java/com/example/apiRestfulworkshopmongoDb/domain/entities/User.java
+++ src/main/java/com/example/apiRestfulworkshopmongoDb/domain/entities/User.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Objects;
 
@@ -11,7 +13,9 @@ import java.util.Objects;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "user")
 public class User {
+    @Id
     private String id;
     private String name;
     private String email;
