c799a0a5a2d3a95458036db4ddf2827357d314a9
Gabriella <gabriellalok3@gmail.com>
2023-03-09 02:48:06 -0300

    inicio do projeto

---
+++ src/main/java/com/example/apiRestfulworkshopmongoDb/ApiRestfulWorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiRestfulworkshopmongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulWorkshopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulWorkshopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiRestfulworkshopmongoDb/ApiRestfulWorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiRestfulworkshopmongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulWorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
