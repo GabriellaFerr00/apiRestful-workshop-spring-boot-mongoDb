8f52a8e2a4d2377bacaabdbb12dc71b754dc654c
Gabriella <gabriellalok3@gmail.com>
2023-03-09 23:30:29 -0300

    insercao de User com metodo POST

--- src/main/java/com/example/apiRestfulworkshopmongoDb/services/UserService.java
+++ src/main/java/com/example/apiRestfulworkshopmongoDb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.apiRestfulworkshopmongoDb.services;
 
 import com.example.apiRestfulworkshopmongoDb.domain.entities.User;
+import com.example.apiRestfulworkshopmongoDb.dto.UserDTO;
 import com.example.apiRestfulworkshopmongoDb.exception.ObjectNotFoundException;
 import com.example.apiRestfulworkshopmongoDb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user){
+       return  userRepository.insert(user);
+    }
+    //esse metodo fere a responsabilidade unica, porem, dedpendendo da situacao para instanciar um user, posso querer acessar
+    //o banco de dados e quem tem essa injecao de dependencia e o userService
+    public User fromDTO(UserDTO dto){
+        return new User(dto.getId(), dto.getName());
     }
 }
