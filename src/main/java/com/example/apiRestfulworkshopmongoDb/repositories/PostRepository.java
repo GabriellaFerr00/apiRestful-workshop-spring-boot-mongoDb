76f022b9fe5464dba7d74084d5afb785ccb7957d
Gabriella <gabriellalok3@gmail.com>
2023-03-10 04:57:02 -0300

    implementando query method findByTitle com anotacao @Query

--- src/main/java/com/example/apiRestfulworkshopmongoDb/repositories/PostRepository.java
+++ src/main/java/com/example/apiRestfulworkshopmongoDb/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.apiRestfulworkshopmongoDb.repositories;
 
 import com.example.apiRestfulworkshopmongoDb.domain.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
 
 }
